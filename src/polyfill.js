a004bebe6f9ec1eb6b98b9ad9af2da5a3bada6fd
shine <ls@gokuai.com>
2020-03-16 16:16:46 +0800

    update

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,3 +1,20 @@
-if (('DOMRect' in window)) {
+if (!('DOMRect' in window)) {
   window.DOMRect = window.ClientRect
+}
+
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    let el = this
+
+    do {
+      if (el.matches(s)) return el
+      el = el.parentElement || el.parentNode
+    } while (el !== null && el.nodeType === 1)
+    return null
+  }
 }
