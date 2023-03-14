e655889310ec104d01a0efde90e8ced61b98ce76
shine <ls@gokuai.com>
2023-03-14 13:41:05 +0800

    upgrade component

--- build/base.config.js
+++ build/base.config.js
@@ -20,6 +20,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       }, {
+        test: /\.js$/,
+        include: /node_modules\/pdfjs-dist/,
+        loader: 'babel-loader'
+      }, {
         test: /\.(sa|sc|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', {
           loader: 'postcss-loader',
